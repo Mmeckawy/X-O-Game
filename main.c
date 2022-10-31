2702343bbade580584dbf78692958323b1b211a0
Mariam Meckawy <69309651+Mmeckawy@users.noreply.github.com>
2022-10-31 23:16:20 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
-#include <conio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-char board [3][3] = { {'1','2','3'},
-                      {'4','5','6'},
-                      {'7','8','9'}};
+char board [3][3] = 
+{ {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'}
+};
+
+char sign = 'X';
+int choice,r,c;
 
 void print_board()
 {
-    system("cls");
     printf(" Player 1: X  -  Player 2: O\n\n");
     for(int i=0; i<3; i++)
     {
@@ -20,102 +25,147 @@ void print_board()
     }
 }
 
-int check_win()
+bool vertical_win()
 {
-    if (board[0][0] == board[0][1] && board[0][1] == board[0][2])
-        return 1;
+  if(board[0][0] == board[1][0] && board[1][0] == board[2][0])
+  return true;
 
-    else if (board[1][0] == board[1][1] && board[1][1] == board[1][2])
-        return 1;
+  else if(board[0][1] == board[1][1] && board[1][1] == board[2][1])
+  return true;
 
-    else if (board[2][0] == board[2][1] && board[2][1] == board[2][2])
-        return 1;
+  else if(board[0][2] == board[1][2] && board[1][2] == board[2][2])
+  return true;
 
-    else if (board[0][0] == board[1][0] && board[1][0] == board[2][0])
-        return 1;
-
-    else if (board[0][1] == board[1][1] && board[1][1] == board[2][1])
-        return 1;
-
-    else if (board[0][2] == board[1][2] && board[1][2] == board[2][2])
-        return 1;
-
-    else if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
-        return 1;
-
-    else if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
-        return 1;
-
-    else if (board[0][0] != '1' && board[0][1]!= '2' && board[0][2] != '3' &&
-        board[1][0] != '4' && board[1][1] != '5' && board[1][2] != '6' &&
-        board[2][0] != '7' && board[2][1] != '8' && board[2][2] != '9')
-
-        return 0;
-    else
-        return  - 1;
+  else return false;
 }
 
-int main()
+bool horizontal_win()
 {
-    int player = 1, i, choice;
+  if(board[0][0] == board[0][1] && board[0][1] == board[0][2])
+  return true;
 
-    char sign;
-    do
-    {
-        print_board();
-        player = (player % 2) ? 1 : 2;
+  else if(board[1][0] == board[1][1] && board[1][1]== board[1][2])
+  return true;
 
-        printf("\n  Player %d, enter number:  ", player);
-        scanf("%d", &choice);
+  else if(board[2][0] == board[2][1] && board[2][1] == board[2][2])
+  return true;
 
-        sign = (player == 1) ? 'X' : 'O';
+  else return false;
+}
 
-        if (choice == 1 && board[0][0] == '1')
-            board[0][0] = sign;
+bool diagonal_win()
+{
+  if(board[0][0] == board[1][1] && board[1][1] == board[2][2])
+  return true;
 
-        else if (choice == 2 && board[0][1] == '2')
-            board[0][1] = sign;
+  else if(board[0][2] == board[1][1] && board[1][1] == board[2][0])
+  return true;
 
-        else if (choice == 3 && board[0][2] == '3')
-            board[0][2] = sign;
+  else return false;
+}
 
-        else if (choice == 4 && board[1][0] == '4')
-            board[1][0] = sign;
+void play_move()
+{
+  if(sign == 'X')
+  {
+  printf(" Player 1 enter choice:\n");
+  }
+  else if(sign == 'O')
+  {
+  printf(" Player 2 enter choice: \n");
+  }
 
-        else if (choice == 5 && board[1][1] == '5')
-            board[1][1] = sign;
+  scanf("%d", &choice);
 
-        else if (choice == 6 && board[1][2] == '6')
-            board[1][2] = sign;
+  switch (choice)
+  {
+  case 1:
+    r = 0, c = 0;
+    break;
 
-        else if (choice == 7 && board[2][0] == '7')
-            board[2][0] = sign;
+  case 2:
+    r = 0, c = 1;
+    break;
 
-        else if (choice == 8 && board[2][1] == '8')
-            board[2][1] = sign;
+  case 3:
+    r = 0, c = 2;
+    break;
 
-        else if (choice == 9 && board[2][2] == '9')
-            board[2][2] = sign;
+case 4:
+    r = 1, c = 0;
+    break;
 
-        else
-        {
-            printf("Invalid move!");
+  case 5:
+    r = 1, c = 1;
+    break;    
 
-            player--;
-            getch();
-        }
-        i = check_win();
+  case 6:
+    r = 1, c = 2;
+    break;
 
-        player++;
-    }while (i ==  - 1);
+case 7:
+    r = 2, c = 0;
+    break;
 
+  case 8:
+    r = 2, c = 1;
+    break;
+
+  case 9:
+    r = 2, c = 2;
+    break;
+
+  default:
+   printf(" Invalid move \n");
+    break;
+  }
+
+  if(sign == 'X' && board[r][c] != 'X' && board[r][c] != 'O')
+  {
+    board[r][c] = 'X';
+    sign = 'O';
+  }
+
+  else if(sign == 'O' && board[r][c] != 'X' && board[r][c] != 'O')
+  {
+    board[r][c] = 'O';
+    sign = 'X';
+  }
+  else 
+  {
+    printf("Box already filled\n");
+    play_move();
+  }
+  print_board();
+}
+
+bool win_game()
+{
+  if(vertical_win() == true || horizontal_win() == true || diagonal_win() == true)
+  return true;
+  else false;
+}
+
+int main() {
+
+  while(win_game() == false)
+  {
     print_board();
+    play_move();
+  }
 
-    if (i == 1)
-        printf("Player %d win ", --player);
-    else
-        printf("Tie between the two players");
+  if(win_game() == true && sign == 'O')
+  {
+    printf("Player 1 won");
+  }
+  else  if(win_game() == true && sign == 'X')
+  {
+    printf("Player 2 won");
+  }
+  else
+  {
+    printf("Draw");
+  }
 
-    getch();
-    return 0;
+  return 0;
 }
